f8f577b88010258cee40d12d1bbdadfe9e4dcec3
Evan Schultz <evan@rangle.io>
2019-03-06 13:56:17 -0500

    test

---
+++ examples/example-import.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function MyThingTwo({input}: {input: Number}) {
+    return <div>Hi Two {input}</div>
+}
--- examples/example1.tsx
+++ examples/example1.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import {  MyThingTwo } from "./example-import";
 export function MyThing({input}: {input: Number}) {
     return <div>Hi {input}</div>
 }
 
 export default function App() {
-    return (<MyThing input={1}/>)
+    return (<><MyThing input={1}/>
+        <MyThingTwo input={2}/>
+        </>
+        )
 }
