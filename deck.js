466e918aa11daed50a1e1725a4ba19a29e7b7e47
Evan Schultz <evan@rangle.io>
2019-03-07 10:19:38 -0500

    [#3] Function Section
    - Intro
    - Arrow Functions
    - Callback Functions
    
    starting content on Pure Functions
    
    - [ ] - Exercises
    - [ ] - Default Params
    - [ ] - Optional Params
    - [ ] - Functions returning Functions
    - [ ] - Destructuring params
    - [ ] - ...rest
    
    notes on context
    
    wip

--- deck.js
+++ deck.js
@@ -4,12 +4,14 @@ import templateLiterals from './template-literals';
 import objects from './objects';
 import styles from './styles';
 import arrays from './arrays';
+import functions from './functions'
 export { default as theme } from './theme'
 
 export default [
     ...intro,
     ...variables,
     ...templateLiterals,
+    ...functions,
     ...objects,
     ...arrays,
     ...styles
---
+++ functions/index.js
@@ -0,0 +1,6 @@
+import intro from "./00-intro.mdx";
+import arrow from "./01-arrow-functions.mdx";
+import callBack from "./02-function-callback.mdx";
+import functionContext from "./03-function-context.mdx";
+export { default as theme } from '../theme'
+export default [...intro, ...arrow, ...callBack, ...functionContext];
