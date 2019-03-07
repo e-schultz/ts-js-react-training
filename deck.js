ac8fa4663b2403224d6a4b76ee63fd425ca47a85
Renee Vrantsidis <renee.vrantsidis@rangle.io>
2019-03-07 13:49:03 -0500

    Added module on styles

--- deck.js
+++ deck.js
@@ -2,11 +2,13 @@ import intro from './intro.mdx'
 import variables from './variables'
 import templateLiterals from './template-literals';
 import objects from './objects';
+import styles from './styles';
 export { default as theme } from './theme'
 
 export default [
     ...intro,
     ...variables,
     ...templateLiterals,
-    ...objects
+    ...objects,
+    ...styles
 ]
