d6a1be52b2ab16c575c8ed6e08858388ef3bd73d
Renee Blackburn <renee.blackburn@rangle.io>
2019-03-08 11:13:20 -0500

    Added module on styles (#6)
    * Added module on styles
    
    * Added resources.md and updated examples

---
+++ RESOURCES.md
@@ -0,0 +1,4 @@
+## Styling
+- https://tachyons.io/#style
+- https://glenmaddern.com/articles/css-modules
+- https://www.youtube.com/watch?v=uHVqbCPnOwU
---
+++ styles/index.js
@@ -0,0 +1,4 @@
+import intro from './00-intro.mdx';
+import cssApproaches from './01-css-approaches.mdx'
+
+export default [...intro, ...cssApproaches];
