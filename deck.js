1aa349c5798df195d6f10ab95f51926b3ddce98e
Renee Vrantsidis <renee.vrantsidis@rangle.io>
2019-03-08 11:07:59 -0500

    Array module

--- deck.js
+++ deck.js
@@ -2,11 +2,13 @@ import intro from './intro.mdx'
 import variables from './variables'
 import templateLiterals from './template-literals';
 import objects from './objects';
+import arrays from './arrays';
 export { default as theme } from './theme'
 
 export default [
     ...intro,
     ...variables,
     ...templateLiterals,
-    ...objects
+    ...objects,
+    ...arrays
 ]
