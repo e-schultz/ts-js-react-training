62e6b576c8963db64a68a9b550b62cf04a461b10
Evan Schultz <evan@rangle.io>
2019-03-09 13:06:23 -0500

    Added Promise Chains
    clean up floating link
    
    fix code example
    
    fix spelling
    
    fix spelling

--- decks/promises/index.js
+++ decks/promises/index.js
@@ -1,3 +1,4 @@
 import intro from './00-promises.mdx';
+import chain from './01-promise-chain.mdx';
 export { default as theme } from '../../theme'
-export default [ ...intro,]
+export default [ ...intro, ...chain]
