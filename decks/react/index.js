93f807a789da72eca03c2700e8dccbe2b0e91a72
Renee Blackburn <renee.vrantsidis@rangle.io>
2019-03-09 13:16:58 -0500

    Added section on Props and WIP section on composition

--- decks/react/index.js
+++ decks/react/index.js
@@ -1,5 +1,13 @@
 import intro from './00-intro.mdx'
 import components from './01-components.mdx'
+import props from './02-props.mdx'
+import composition from './03-composition.mdx'
 import styles from './styles'
 export { default as theme } from '../../theme'
-export default [...intro, ...components, ...styles]
+export default [
+  ...intro,
+  ...components,
+  ...props,
+  ...composition,
+  ...styles
+]
