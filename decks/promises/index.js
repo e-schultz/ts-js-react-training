4f9947d383169aeb3a9bd57f48e67e249f059be7
Evan Schultz <evan@rangle.io>
2019-03-09 19:42:39 -0500

    Added Promise Chains (#16)
    * Added Promise Chains
    * fix spelling of Bluebird
    * Create ReplIt component
    * error handling
    * Promise Errors

--- decks/promises/index.js
+++ decks/promises/index.js
@@ -1,3 +1,6 @@
 import intro from './00-promises.mdx';
+import chain from './01-promise-chain.mdx';
+import errors from './02-promise-errors.mdx';
+import patterns from './03-promise-patterns.mdx';
 export { default as theme } from '../../theme'
-export default [ ...intro,]
+export default [ ...intro, ...chain, ...errors, ...patterns]
