dfd2fce6e016cbd0d3c4cf5839a809445fbf6392
Evan Schultz <evan@rangle.io>
2019-03-10 12:13:41 -0400

    expand closure and re-arrange slides

--- decks/functions/index.js
+++ decks/functions/index.js
@@ -1,6 +1,17 @@
 import intro from "./00-intro.mdx";
-import arrow from "./01-arrow-functions.mdx";
-import functionContext from "./02-function-context.mdx";
-import callBack from "./03-function-callback.mdx";
-export { default as theme } from '../../theme'
-export default [...intro, ...arrow,  ...functionContext, ...callBack];
+
+import functionScopeIntro from "./01-function-scope-closure-context.mdx";
+import functionClosure from "./02-function-closure.mdx";
+import functionContext from "./03-function-context-this.mdx";
+import arrow from "./04-arrow-functions.mdx";
+import callBack from "./05-function-callback.mdx";
+export { default as theme } from "../../theme";
+
+export default [
+  ...intro,
+  ...functionScopeIntro,
+  ...functionClosure,
+  ...functionContext,
+  ...arrow,
+  ...callBack
+];
