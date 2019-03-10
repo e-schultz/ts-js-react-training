b7203209f5d4c1ef98f2bb4758369fa835cc6fde
Evan Schultz <evan@rangle.io>
2019-03-09 22:22:36 -0500

    wip

---
+++ decks/react/snippets/hello-world-1.snippet.js
@@ -0,0 +1,7 @@
+() => {
+  const HelloWorld = () => {
+    return <div>Hello world!</div>;
+  };
+
+  return (<HelloWorld/>)
+};
---
+++ decks/react/snippets/hello-world-2.snippet.js
@@ -0,0 +1,5 @@
+() => {
+  // You can also use implicit returns
+  const HelloWorld = () => <div>Hello world!</div>;
+  return <HelloWorld />;
+};
