99653f1940630d672032c163d78eb14996dc6b98
Evan Schultz <evan@rangle.io>
2019-03-09 19:41:05 -0500

    Promise Errors

--- decks/promises/index.js
+++ decks/promises/index.js
@@ -1,4 +1,6 @@
 import intro from './00-promises.mdx';
 import chain from './01-promise-chain.mdx';
+import errors from './02-promise-errors.mdx';
+import patterns from './03-promise-patterns.mdx';
 export { default as theme } from '../../theme'
-export default [ ...intro, ...chain]
+export default [ ...intro, ...chain, ...errors, ...patterns]
