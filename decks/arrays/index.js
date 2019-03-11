cea5b636d4f01437606ac84336248e4a2e005c99
Evan Schultz <evan@rangle.io>
2019-03-11 02:35:56 -0400

    add two array exercises (#30)

--- decks/arrays/index.js
+++ decks/arrays/index.js
@@ -1,5 +1,6 @@
 import intro from './00-intro.mdx';
 import iterating from './01-iterating.mdx';
 import spreadDestructuring from './02-spread-destructure.mdx'
+import arrayHomework from './99-array-exercises.mdx'
 export { default as theme } from '../../theme'
-export default [ ...intro, ...iterating, ...spreadDestructuring]
+export default [ ...intro, ...iterating, ...spreadDestructuring, ...arrayHomework]
