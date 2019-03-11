0cb4ea63b292e24e691b21be19dac434b5b1adca
Evan Schultz <evan@rangle.io>
2019-03-10 20:01:55 -0400

    Fix CodeSurf
    Looks like using the `<LiveCode` component was causing issues with how  `<CodeSurf>` was loading files.
    
    Moved some examples to CodeSandbox - it gives people a spot to play with them that persists after (instead of being only in the browser)

---
+++ components/codesandbox.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function CodeSandbox({ src, allowFullScreen = true }) {
+  return (
+    <div style={{ height: "75vh", display: "block" }}>
+
+      <iframe
+        height="100%"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: 0,
+          borderRadius: "4px",
+          overflow: "hidden"
+        }}
+        width="100%"
+        src={src}
+        sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+      />
+    </div>
+  );
+}
