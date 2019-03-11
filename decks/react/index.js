81c9820f7d1e8220208ad0e34180d032810bea5d
Evan Schultz <evan@rangle.io>
2019-03-10 20:22:27 -0400

    Fix CodeSurf (#26)
    Looks like using the `<LiveCode` component was causing issues with how  `<CodeSurf>` was loading files.
    
    Moved some examples to CodeSandbox - it gives people a spot to play with them that persists after (instead of being only in the browser)

--- decks/react/index.js
+++ decks/react/index.js
@@ -2,12 +2,11 @@ import intro from './00-intro.mdx'
 import components from './01-components.mdx'
 import props from './02-props.mdx'
 import composition from './03-composition.mdx'
-import styles from './styles'
+
 export { default as theme } from '../../theme'
 export default [
   ...intro,
   ...components,
   ...props,
-  ...composition,
-  ...styles
+  ...composition
 ]
--- decks/styles/index.js
+++ decks/styles/index.js
@@ -1,4 +1,7 @@
 import intro from './00-intro.mdx';
 import cssApproaches from './01-css-approaches.mdx'
 
+export { default as theme } from '../../theme'
+
+
 export default [...intro, ...cssApproaches];
