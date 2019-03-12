e49af53a6b1b710ac9fc2e876a42429ff0e6373c
Evan Schultz <evan@rangle.io>
2019-03-12 00:19:33 -0400

    Add class content and subclass content

--- decks/classes/index.js
+++ decks/classes/index.js
@@ -1,4 +1,6 @@
 import intro from './00-intro.mdx';
+import subcassing from './01-subclassing.mdx';
 
 export { default as theme } from '../../theme'
-export default [ ...intro]
+
+export default [ ...intro,...subcassing]
