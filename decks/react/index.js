5c004194de80b3736c9f139a0fa95bca12cf13d1
Evan Schultz <evan@rangle.io>
2019-03-13 02:30:39 -0400

    flesh out react

--- decks/react/index.js
+++ decks/react/index.js
@@ -3,10 +3,13 @@ import components from './01-components.mdx'
 import props from './02-props.mdx'
 import composition from './03-composition.mdx'
 
+import mediaCard from './04-exercise-1-media-card.mdx'
+
 export { default as theme } from '../../theme'
 export default [
   ...intro,
   ...components,
   ...props,
-  ...composition
+  ...composition,
+  ...mediaCard
 ]
