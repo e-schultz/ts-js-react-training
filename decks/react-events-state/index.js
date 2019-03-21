96dd6b86ea3949b39dec0502ef5767463705c1a1
Evan Schultz <evan@rangle.io>
2019-03-21 16:37:38 -0400

    Reorder Class Content
    * Events
    * Class Based Components - basics
    * LifeCycle - three stages, commonly used
    * State & Data Flow
    * Data Fetch exercise

--- decks/react-events-state/index.js
+++ decks/react-events-state/index.js
@@ -1,11 +1,13 @@
 import events from './06-events.mdx'
-import lifeCycle from './07-react-life-cycle.mdx';
-import dataFlow from './08-react-state-and-data-flow.mdx';
+import classBased from './07-class-based-components.mdx';
+import lifeCycle from './08-react-life-cycle.mdx';
+import dataFlow from './09-react-state-and-data-flow.mdx'
 import dataFetch from './99-data-fetch-exercises.mdx';
 export { default as theme } from '../../theme'
 
 export default [
   ...events,
+  ...classBased,
   ...lifeCycle,
   ...dataFlow,
   ...dataFetch
