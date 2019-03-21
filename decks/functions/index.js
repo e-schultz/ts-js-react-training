576b45947f9d296f77aa135750839c6c3e9f22de
Evan Schultz <evan@rangle.io>
2019-03-21 15:57:42 -0400

    Function Cleanup (#69)
    - Since we cover closures before scope and context - clean up file names and order
    - Gave another example of a use case for functions returning functions
    - Updated index to reflect file name changes

--- decks/functions/index.js
+++ decks/functions/index.js
@@ -1,6 +1,6 @@
 import intro from "./00-intro.mdx";
 import pureFunctions from "./01-pure-functions.mdx"
-import functionScopeIntro from "./05-function-scope-closure-context.mdx";
+import functionScopeIntro from "./05-function-scope-context.mdx";
 import functionClosure from "./04-function-closure.mdx";
 import functionContext from "./06-function-context-this.mdx";
 import arrow from "./02-arrow-functions.mdx";
