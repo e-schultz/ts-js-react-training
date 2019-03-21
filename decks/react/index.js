56b785fec2db3505c25aaea8b8df573c3e529cd6
Evan Schultz <evan@rangle.io>
2019-03-21 10:02:02 -0400

    Remove duplicate slides & Reorder React Content (#62)
    * Remove Duplicate HelloWorld Slide
    
    * Inline jsx block was repeating what was on following code-sandbbox
    
    * Combine Basic and With Children into one code sandbox
    
    * Instead of having two CodeSandbox to show HelloWorld, and HelloWorld with Children - combined into one
    
    * Reorder React Content
    
    I did some shifting around of content and breaking out lists / conditionals into their own slide blocks.
    
    Also combined a few codepens that were two different ones, into one if they were simple enough
    
    Changed - https://codesandbox.io/s/3kv214881q?from-embed
    
    To not loop over the names yet - since we haven't covered list rendering yet
    
    Then shifted the order to be
    
    - props
    - rendering lists
    - conditional rendering
    - composition

--- decks/react/index.js
+++ decks/react/index.js
@@ -1,12 +1,14 @@
 import intro from './00-intro.mdx'
 import components from './01-components.mdx'
 import props from './02-props.mdx'
-import composition from './03-composition.mdx'
-import listsConditionals from './04-lists-and-conditions.mdx'
-import mediaCard from './05-exercise-1-media-card.mdx'
+import renderingLists from './03-rendering-lists.mdx'
+import conditionalRendering from './04-conditional-rendering.mdx'
+import composition from './05-00-composition.mdx'
+import mediaCard from './05-01-exercise-1-media-card.mdx'
+import productCard from './05-02-exercise-2-product-card.mdx'
 import events from './06-events.mdx'
-import dataFlow from './08-react-state-and-data-flow.mdx';
 import lifeCycle from './07-react-life-cycle.mdx';
+import dataFlow from './08-react-state-and-data-flow.mdx';
 import dataFetch from './99-data-fetch-exercises.mdx';
 export { default as theme } from '../../theme'
 
@@ -14,9 +16,11 @@ export default [
   ...intro,
   ...components,
   ...props,
+  ...renderingLists,
+  ...conditionalRendering,
   ...composition,
-  ...listsConditionals,
   ...mediaCard,
+  ...productCard,
   ...events,
   ...lifeCycle,
   ...dataFlow,
