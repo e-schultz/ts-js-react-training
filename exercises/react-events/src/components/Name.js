9fdb84137bc6612a7ae4211c19c8ffa5c3b64418
Renee Blackburn <renee.vrantsidis@rangle.io>
2019-03-25 10:14:21 -0400

    added exercises

---
+++ exercises/react-events/src/components/Name.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Name = () => {
+  // Complete this component so that the input value will be logged in the console while you type.
+  // Then, make it so that when the button is pressed, "Hello, {name}!" will be logged in the console.
+  return (
+    <div>
+      <input type="text" onInput={} />
+      <button type="button" onClick={}>Hello!</button>
+    </div>
+  )
+}
+
+export default Name
