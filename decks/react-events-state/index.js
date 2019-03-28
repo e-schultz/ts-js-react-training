3673b4ef058a70f8cfa6737f5b7bfef8094f5e49
Evan Schultz <evan@rangle.io>
2019-03-27 23:05:39 -0400

    add data fetch example

--- decks/react-events-state/index.js
+++ decks/react-events-state/index.js
@@ -3,8 +3,9 @@ import dataFlow from './02-react-state-and-data-flow.mdx'
 import classBased from './03-class-based-components.mdx';
 import stateExercise from './stateful-component-exercise.mdx'
 import lifeCycle from './04-react-life-cycle.mdx';
-import hooks from './05-hooks.mdx'
-import dataFetch from './99-data-fetch-exercises.mdx';
+import hooks from './06-hooks.mdx'
+import dataFetch from './05-data-fetch.mdx'
+import dataFetchE from './99-data-fetch-exercises.mdx';
 
 export { default as theme } from '../../theme'
 
@@ -14,6 +15,7 @@ export default [
   ...classBased,
   ...stateExercise,
   ...lifeCycle,
+  ...dataFetch,
   ...hooks,
-  ...dataFetch
+  ...dataFetchE
 ]
