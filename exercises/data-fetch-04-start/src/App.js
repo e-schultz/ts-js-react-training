6e569fafc507d83f0b9711278e1064de422638f2
Evan Schultz <evan@rangle.io>
2019-03-27 22:21:43 -0400

    Update Data-Fetch 4
    * Move `SearchBar` to be outside of search results

--- exercises/data-fetch-04-start/src/App.js
+++ exercises/data-fetch-04-start/src/App.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import HackerNewsSearch from './components/HackerNewsSearch';
+import React, { Component } from "react";
+import "./App.css";
+import HackerNewsSearch from "./components/HackerNewsSearch";
+import SearchBar from "./components/SearchBar";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: "react"
+    };
+  }
+  handleSearch(event) {
+    this.setState({
+      query: event
+    })
+  }
   render() {
     return (
       <div className="App">
-        <HackerNewsSearch />
+        <SearchBar
+          initialQuery={this.state.query}
+          onSearch={event => this.handleSearch(event)}
+        />
+        <HackerNewsSearch query={this.state.query}/>
       </div>
     );
   }
