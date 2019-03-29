a972ef21d764ffad83eb0e94a45eae6bcb687696
Evan Schultz <evan@rangle.io>
2019-03-29 11:00:59 -0400

    React Performance - Initial Info (#79)

---
+++ decks/react-performance/index.js
@@ -0,0 +1,5 @@
+import intro from './00-intro.mdx';
+import largeData from './01-rendering-large-data.mdx'
+
+export { default as theme } from '../../theme'
+export default [ ...intro, ...largeData]
