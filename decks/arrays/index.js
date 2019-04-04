838fff3b1643e2848d10fb389726f2b0550d9687
Evan Schultz <evan@rangle.io>
2019-04-04 11:58:45 -0400

    Updating Array Content
    * Trying to make this a bit more of a 'FP 101'
    * Adjusting examples to use similar data structure so they can build off of previous concepts better
    
    TODO:
    - exercise for each smaller step
    - exercise that combines them all

--- decks/arrays/index.js
+++ decks/arrays/index.js
@@ -1,5 +1,6 @@
 import intro from './00-intro.mdx';
-import iterating from './01-iterating.mdx';
+import arrayMutation  from './01-array-modification-and-mutation.mdx';
+import iterating from './02-iterating.mdx';
 import arrayHomework from './99-array-exercises.mdx'
 export { default as theme } from '../../theme'
-export default [ ...intro, ...iterating, ...arrayHomework]
+export default [ ...intro, ...arrayMutation, ...iterating, ...arrayHomework]
