2a3ae774b83844f86635daca7f3b4959537b354e
Evan Schultz <evan@rangle.io>
2019-04-16 15:42:55 -0400

    remove data fetch import

--- decks/react-events-state/index.js
+++ decks/react-events-state/index.js
@@ -4,9 +4,6 @@ import dataFlow from './03-react-state-and-data-flow.mdx'
 import stateExercise from './stateful-component-exercise.mdx'
 import lifeCycle from './04-react-life-cycle.mdx';
 
-import dataFetch from './05-data-fetch.mdx'
-import dataFetchE from './99-data-fetch-exercises.mdx';
-
 export { default as theme } from '../../theme'
 
 export default [
