4585bc3859153ceb2fc7930737cf9b24bfba5642
Evan Schultz <evan@rangle.io>
2019-04-16 14:06:19 -0400

    [#100] Reorder Event State Flow
    * Move Class to start of deck
    * Rename / re-order files to match new order
    * For example - starting with function `hello world` - will refactor / rewrite to class based and manage state

--- examples/react-basic-class-component/src/App.js
+++ examples/react-basic-class-component/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorldCls'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HelloWorld/>
+        <HelloWorld text={'Hello World'} />
       </div>
     );
   }
--- examples/react-basic-class-component/src/components/HelloWorld.js
+++ examples/react-basic-class-component/src/components/HelloWorld.js
@@ -1,33 +1,15 @@
 import React from "react";
 
-class HelloWorld extends React.Component {
-  state = {
-    text: "Hello World",
-    count: 0
-  };
-
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(state => {
-      let count = state.count;
-      count++;
-      return {
-        count
-      };
-    });
-  }
-  render() {
-    return (
-      <section>
-        {`${this.state.text} - clicked ${this.state.count} times`}
-        <button onClick={this.handleClick}>Click Me</button>
-      </section>
-    );
-  }
+function HelloWorld(props) {
+  let count = 0;
+ 
+  return (
+    <section>
+      {`${props.text} - clicked ${count} times`}
+      <button>Click Me</button>
+    </section>
+  ); 
 }
+
 
 export default HelloWorld;
