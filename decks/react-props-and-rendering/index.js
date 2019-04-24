2134dc5cee76d280c692c0517d58a9c407a41615
Evan Schultz <evan@rangle.io>
2019-04-24 08:23:59 -0400

    Reorder List Exercise
    * Move Robot Card List to after 'rendering lists'
    * Move shopping cart list to end

--- decks/react-props-and-rendering/index.js
+++ decks/react-props-and-rendering/index.js
@@ -1,9 +1,11 @@
 import props from './01-props.mdx';
 import composiiton from './02-composition.mdx';
-import lists from './03-rendering-lists.mdx';
+import lists from './03-00-rendering-lists.mdx';
 import condition from './04-conditional-rendering.mdx';
-import exercise from './05-01-exercise-1-media-card.mdx'
-import exercise2 from './05-02-exercise-2-product-card.mdx'
+import exercise from './03-01-exercise-1-media-card.mdx';
+import exercise2 from './05-02-exercise-2-product-card.mdx';
+import exercise3 from './05-03-exercise-3-shopping-list.mdx';
 
 export { default as theme } from '../../theme'
-export default [ ...props, ...composiiton, ...lists, ...condition, ...exercise, ...exercise2]
+
+export default [ ...props, ...composiiton, ...lists, ...exercise, ...condition, ...exercise2, ...exercise3]
