3ffef17957a11054a9d67a610f75dadf41a3dd4a
Evan Schultz <evan@rangle.io>
2019-05-02 16:32:39 -0400

    SelectList - let consumer specify extra options
    The SelectList was also taking control over rendering the 'Select One', or 'Any' type of option.
    
    So on the search screen, and in the robot form were displaying
    
    ```jsx
     <option value={-1}>Select One</option>
    ````
    
    We want the consumer to control how to display this, and if we want to display it or not.
    
    We could in theory push an item into the options passed in and modify the array, but that feels clunky

--- examples/react-robo-hr/src/lib/components/SelectList.js
+++ examples/react-robo-hr/src/lib/components/SelectList.js
@@ -3,13 +3,14 @@ const SelectList = ({
   options,
   value,
   onChange,
+  children,
   config = { keyKey: "id", valueKey: "id", textKey: "text" }
 }) => {
   let { keyKey, valueKey, textKey } = config;
   return (
     <span className="select">
       <select value={value} onChange={onChange}>
-        <option value={-1}>Select One</option>
+        {children}
         {options.map(option => {
           return (
             <option key={option[keyKey]} value={option[valueKey]}>
