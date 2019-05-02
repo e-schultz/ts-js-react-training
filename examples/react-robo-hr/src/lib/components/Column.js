23a1ff9223e03915aa1a633f0a49576bdea5a88f
Evan Schultz <evan@rangle.io>
2019-05-02 15:01:03 -0400

    Flesh out column sizing

--- examples/react-robo-hr/src/lib/components/Column.js
+++ examples/react-robo-hr/src/lib/components/Column.js
@@ -3,14 +3,25 @@ import classNames from "classnames";
 
 // todo - have easier api to specify size/offset without needing to look up bulma docss
 
+const getSize = (size) => typeof size === 'number' ? parseFloat(size.toFixed(2)) : undefined
 const Column = ({
   className,
   size,
   children,
   ...props
 }) => {
+  const normalizedSize = getSize(size)
   var columnClass = classNames('column', className, {
-    'is-one-third': size && typeof size === 'number' && size.toFixed(2) === '0.33'
+    'is-one-third': normalizedSize === 0.33,
+    'is-three-quarters': normalizedSize === 0.75,
+    'is-half': normalizedSize === 0.5,
+    'is-one-quater': normalizedSize === 0.25,
+    'is-four-fiths': normalizedSize === 0.8,
+    'is-three-fiths': normalizedSize === 0.6,
+    'is-two-fiths': normalizedSize === 0.4,
+    'is-one-fith': normalizedSize === 0.2,
+    'is-full': normalizedSize === 1,
+
   });
   return <div className={columnClass}>{children}</div>;
 };
