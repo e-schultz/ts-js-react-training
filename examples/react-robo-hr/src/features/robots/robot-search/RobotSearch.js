43f04183898b2be5e02d09a2b454d2de9259affe
Evan Schultz <evan@rangle.io>
2019-05-02 15:13:56 -0400

    Lift RobotState
    I know I'll want to use some of the RobotData on the dashboard
    
    Moved the robot list / reducer to the top level container, and pass down the robot and the dispatcher to update them to the child.
    
    Building the robot so it has the extra props on it is going to be consistent across the app - so we leave the building here for now.
    
    The filtering logic is specific to the RobotSearch - so we leave that in the RobotSearch

--- examples/react-robo-hr/src/features/robots/robot-search/RobotSearch.js
+++ examples/react-robo-hr/src/features/robots/robot-search/RobotSearch.js
@@ -6,50 +6,16 @@ import RobotForm from "../components/RobotForm";
 import RobotSearchBar from "../components/RobotSearchBar";
 import RobotSearchCard from "../components/RobotSearchCard";
 
-import { departments, jobTitles, INITIAL_ROBOTS } from "../../../data";
 
-
-const robotReducer = (state, action) => {
-  switch (action.type) {
-    case "SAVE":
-      return state.map(robot => {
-        return robot.id !== action.robot.id ? robot : { ...action.robot };
-      });
-    default:
-      return state;
-  }
-};
-
-const robotFactory = (departments, jobTitles) => (robot) => {
-  let jobTitle = jobTitles.find(
-    title => title.id === robot.jobTitleId
-  );
-  let department = departments.find(
-    department => department.id === jobTitle.departmentId
-  );
-
-  return {
-    ...robot,
-    jobTitle: jobTitle.text,
-    departmentId: department.id,
-    department: department.text
-  };
-}
-const robotBuilder = robotFactory(departments, jobTitles);
-const createSearchFilter = (searchTerm) => (robot) => {
-  if (
-    typeof searchTerm === "undefined" ||
-    searchTerm.trim().length === 0
-  ) {
+const createSearchFilter = searchTerm => robot => {
+  if (typeof searchTerm === "undefined" || searchTerm.trim().length === 0) {
     return true;
   } else {
     for (let key in robot) {
       console.log(key, robot[key]);
       if (typeof robot[key] === "string") {
         if (
-          robot[key]
-            .toLowerCase()
-            .indexOf(searchTerm.toLocaleLowerCase()) >= 0
+          robot[key].toLowerCase().indexOf(searchTerm.toLocaleLowerCase()) >= 0
         ) {
           return true;
         }
@@ -57,19 +23,18 @@ const createSearchFilter = (searchTerm) => (robot) => {
     }
     return false;
   }
-}
-let createDepartmentFilter = (departmentId) => (robot) => {
-  return departmentId === -1
-                ? true
-                : robot.departmentId === departmentId;
-}
-const RobotSearch = () => {
+};
+let createDepartmentFilter = departmentId => robot => {
+  return departmentId === -1 ? true : robot.departmentId === departmentId;
+};
+const RobotSearch = ({ departments, robots, robotDispatch, jobTitles }) => {
   let [selectedDepartmentId, setSelectedDepartmentId] = useState(-1);
   let [searchTerm, setSearchTerm] = useState("");
   let [selectedRobot, setSelectedRobot] = useState(null);
-  let [robots, robotDispatch] = useReducer(robotReducer, INITIAL_ROBOTS);
+  
   let robotSearchFilter = createSearchFilter(searchTerm);
   let departmentFilter = createDepartmentFilter(selectedDepartmentId);
+  
   return (
     <Section>
       <RobotSearchBar
@@ -83,18 +48,23 @@ const RobotSearch = () => {
       <Columns>
         <Column size={1 / 3}>
           {robots
-            .map(robot => robotBuilder(robot))
-            .filter(robot=>departmentFilter(robot))
-            .filter(robot=>robotSearchFilter(robot))
+            .filter(robot => departmentFilter(robot))
+            .filter(robot => robotSearchFilter(robot))
             .map(robot => {
-              return <RobotSearchCard robot={robot} onEdit={()=>setSelectedRobot(robot)}/>
-             
+              return (
+                <RobotSearchCard
+                  robot={robot}
+                  onEdit={() => setSelectedRobot(robot)}
+                />
+              );
             })}
         </Column>
         <Column size={1 / 3}>
           {!selectedRobot ? null : (
             <RobotForm
               robot={selectedRobot}
+              departments={departments}
+              jobTitles={jobTitles}
               onSubmit={action => {
                 setSelectedRobot(null);
                 robotDispatch(action);
