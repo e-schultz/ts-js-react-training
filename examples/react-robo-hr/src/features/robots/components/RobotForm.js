c7d2971105b484846a710e7cbd1c121cb8bf3c85
Evan Schultz <evan@rangle.io>
2019-05-02 11:20:48 -0400

    Create Robot Feature Module
    * Create Robot feature module
    * Setup routing

---
+++ examples/react-robo-hr/src/features/robots/components/RobotForm.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import Card from "../../../lib/components/Card";
+import CardImage from "../../../lib/components/CardImage";
+import CardContent from "../../../lib/components/CardContent";
+import CardaMediaContent from "../../../lib/components/CardMediaContent";
+import CardFooter from "../../../lib/components/CardFooter";
+import CardFooterItem from "../../../lib/components/CardFooterItem";
+import SelectList from "../../../lib/components/SelectList";
+import InputField from "../../../lib/components/InputField";
+
+// TODO: have departments and jobTitles be provided from parent
+
+import { departments, jobTitles } from "../../../data";
+
+const RobotForm = ({ robot, onSubmit }) => {
+  const [editRobot, setRobot] = useState({ ...robot });
+  return (
+    <Card>
+      <CardImage src={`https://robohash.org/${editRobot.id}?size=1280x960`} />
+      <CardContent>
+        <CardaMediaContent
+          src={`https://robohash.org/${editRobot.id}?size=96x96`}
+        >
+          <InputField
+            type="text"
+            placeholder="Robot Name"
+            value={editRobot.name}
+            onChange={({ target: { value } }) =>
+              setRobot(state => ({
+                ...state,
+                name: value
+              }))
+            }
+          />
+
+          <InputField
+            placeholder="UserName"
+            value={editRobot.username}
+            onChange={({ target: { value } }) =>
+              setRobot(state => ({
+                ...state,
+                username: value
+              }))
+            }
+          />
+        </CardaMediaContent>
+        <SelectList
+          options={departments}
+          value={editRobot.departmentId}
+          onChange={({ target: { value } }) =>
+            setRobot(state => ({
+              ...state,
+              departmentId: +value,
+              jobTitleId: -1
+            }))
+          }
+        />
+
+        <SelectList
+          options={jobTitles.filter(
+            ({ departmentId }) => departmentId === editRobot.departmentId
+          )}
+          value={editRobot.jobTitleId}
+          onChange={({ target: { value } }) =>
+            setRobot(state => ({
+              ...state,
+              jobTitleId: +value
+            }))
+          }
+        />
+      </CardContent>
+      <CardFooter>
+        <CardFooterItem>
+          <button
+            className="button is-fullwidth is-primary"
+            onClick={() => {
+              onSubmit({ type: "SAVE", robot: editRobot });
+            }}
+          >
+            Save
+          </button>
+        </CardFooterItem>
+        <CardFooterItem>
+          <button
+            className="button is-fullwidth is-normal"
+            onClick={() => {
+              onSubmit({ type: "CANCEL" });
+            }}
+          >
+            Cancel
+          </button>
+        </CardFooterItem>
+      </CardFooter>
+    </Card>
+  );
+};
+
+RobotForm.displayName = "RobotForm";
+export default RobotForm;
