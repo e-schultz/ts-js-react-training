bb1a07fad8cca283de4b8d7c6de89430002a35e9
Evan Schultz <evan@rangle.io>
2019-05-02 17:11:48 -0400

    Setup Hamburger Menu
    The Bulma navbar is responsive by default and will collapse down to a hamburger menu on smaller screen.
    
    The Bulma documentation gave this example to setup the toggle using vanilla js
    
    ```js
    document.addEventListener('DOMContentLoaded', () => {
    
      // Get all "navbar-burger" elements
      const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
    
      // Check if there are any navbar burgers
      if ($navbarBurgers.length > 0) {
    
        // Add a click event on each of them
        $navbarBurgers.forEach( el => {
          el.addEventListener('click', () => {
    
            // Get the target from the "data-target" attribute
            const target = el.dataset.target;
            const $target = document.getElementById(target);
    
            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
            el.classList.toggle('is-active');
            $target.classList.toggle('is-active');
    
          });
        });
      }
    
    });
    ```
    
    We could copy/paste this into the index.html and it would work - but we are using React, so lets leverage react.
    
    The SiteHeader was not using any state at all before - but now we need to keep track of if the menu is expanded or not.
    
    We will use that to drive if the `is-active` class is applied
    
    We use the `onClick` on the burger menu icon to toggle if it's expanded or not - and use the `useState` to keep track of the state

--- examples/react-robo-hr/src/lib/components/SiteHeader.js
+++ examples/react-robo-hr/src/lib/components/SiteHeader.js
@@ -1,66 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 import { Link } from "@reach/router";
-// TODO: Don't think this is the right home for this - meant as a 'use-once' kind of thing 
+// TODO: Don't think this is the right home for this - meant as a 'use-once' kind of thing
 // find a better place for it later.
 
-const SiteHeader = () => (
-<nav className="navbar" role="navigation" aria-label="main navigation">
-       {/* <div className="navbar-brand">
-          <a className="navbar-item" href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              width="112"
-              height="28"
-            />
-          </a>
+const SiteHeader = () => {
+  const [burgerExpanded, setBurgerExpanded] = useState(false);
 
-          <a
-            role="button"
-            className="navbar-burger burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-          </a>
-  </div>*/}
+  const burgerClass = classNames("navbar-burger", "burger", {
+    "is-active": burgerExpanded
+  });
+  const navClass = classNames("navbar-menu", {
+    "is-active": burgerExpanded
+  });
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <a
+        role="button"
+        className={burgerClass}
+        aria-label="menu"
+        aria-expanded="false"
+        onClick={() => setBurgerExpanded(state => !state)}
+      >
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </a>
 
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            
-            <Link to="/" className="navbar-item">Dashboard</Link>
-            
+      <div className={navClass}>
+        <div className="navbar-start">
+          <Link to="/" className="navbar-item">
+            Dashboard
+          </Link>
 
-            <Link to="/robots" className="navbar-item">Robots</Link>
+          <Link to="/robots" className="navbar-item">
+            Robots
+          </Link>
 
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">More</a>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">More</a>
 
-              <div className="navbar-dropdown">
-                <a className="navbar-item">About</a>
-                <a className="navbar-item">Jobs</a>
-                <a className="navbar-item">Contact</a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">Report an issue</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <a className="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
-                <a className="button is-light">Log in</a>
-              </div>
+            <div className="navbar-dropdown">
+              <a className="navbar-item">About</a>
+              <a className="navbar-item">Jobs</a>
+              <a className="navbar-item">Contact</a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">Report an issue</a>
             </div>
           </div>
         </div>
-      </nav>);
 
-SiteHeader.displayName = 'SiteHader';
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">Log in</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+SiteHeader.displayName = "SiteHader";
 
 export default SiteHeader;
