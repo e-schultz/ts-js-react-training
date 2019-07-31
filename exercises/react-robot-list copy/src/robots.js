b73ebd17418840f10681ed0dab3087bddcfefba3
Evan Schultz <evan@rangle.io>
2019-07-30 20:22:56 -0400

    moving robot exercise to github

---
+++ exercises/react-robot-list copy/src/robots.js
@@ -0,0 +1,72 @@
+export const ROBOT_LIST = [
+  {
+    id: 1,
+    name: "Leanne Grahamss",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    occupation: "M1A1 Tank Crewman"
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    occupation: "Wellness Manager"
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    occupation: "Synchronous Motor Assembler"
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+    occupation: "Electrical Systems Drafter"
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+    occupation: "M1A1 Tank Crewman"
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+    occupation: "Fabric Pattern Grader"
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+    occupation: "Analytical Statistician"
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+    occupation: "Control System Computer Scientist"
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+    occupation: "Programming Methodology and Languages Researcher"
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+    occupation: "Electrical Systems Drafter"
+  }
+];
